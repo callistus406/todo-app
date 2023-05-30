3e16bf649ee19013a0e4885b2be08fed0fe64515
CALLISTUS <callistus406@gmail.com>
2023-05-30 15:55:49 -0700

    updated update_todo

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("add/todo", views.add_todo, name="add-todo"),
-    path("update/todo", views.update_todo, name="update-todo"),
+    path("update/todo/<int:todo_id>/", views.update_todo, name="update-todo"),
     path("delete/todo/<int:todo_id>/", views.delete_todo, name="delete-todo"),
     path("fetch/all/todo", views.fetch_all_todo, name="fetch-all-todo"),
     path("fetch/todo/<int:todo_id>/", views.fetch_one_todo, name="fetch-todo")
