bd6a4866d8bf63c532d0429df6294afdfa865f7d
CALLISTUS <callistus406@gmail.com>
2023-05-30 14:03:42 -0700

    added new files

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("add/todo", views.add_todo, name="add-todo"),
-    path("update/todo", views.update_todo, name="update-todo")
+    path("update/todo", views.update_todo, name="update-todo"),
+    path("delete/todo/<int:todo_id>/", views.delete_todo, name="delete-todo")
+    # path("update/todo", views.update_todo, name="update-todo"),
 ]
