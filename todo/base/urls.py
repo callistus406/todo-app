67258017f977db3c9e88751e01ab60a8690aeaa7
CALLISTUS <callistus406@gmail.com>
2023-05-30 15:20:43 -0700

    added new routes

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("add/todo", views.add_todo, name="add-todo"),
     path("update/todo", views.update_todo, name="update-todo"),
-    path("delete/todo/<int:todo_id>/", views.delete_todo, name="delete-todo")
-    # path("update/todo", views.update_todo, name="update-todo"),
+    path("delete/todo/<int:todo_id>/", views.delete_todo, name="delete-todo"),
+    path("fetch/all/todo", views.fetch_all_todo, name="fetch-all-todo"),
+    path("fetch/todo/<int:todo_id>/", views.fetch_one_todo, name="fetch-todo")
 ]
