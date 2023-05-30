3b7690a97e6a9860f98c2f3f38051b96a7e5427d
CALLISTUS <callistus406@gmail.com>
2023-05-30 14:03:17 -0700

    created a new model

--- todo/base/models.py
+++ todo/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(default="pending", max_length=12)
+    priority = models.CharField(max_length=10, default="normal")
+    start_date = models.DateField()
+    end_date = models.DateField()
